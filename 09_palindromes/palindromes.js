5e8aebc1fc38be2caeb5a0a06e19c0766d2a7700
Ben Batchelder <bbatchelder.proton.me>
2024-07-19 12:04:26 -0600

    Finished 09 & 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (theWord) {
+    const listOfCharacters = 'abcdefghijklmnopqrstuvwxyz0123456789'
 
+    const goodWord = theWord
+    .toLowerCase()
+    .split('')
+    .filter((letter) => listOfCharacters.includes(letter))
+    .join('');
+
+    const reversedWord = goodWord.split('').reverse().join('');
+
+    return reversedWord === goodWord;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+let startingNumber = -1;
+let secondNumber = 1;
+let newNumber = 0;
 
+    if (number < 0) {
+        return 'OOPS'
+    }
+    else {
+    for (let i = 0; i <= number; i++) {
+        newNumber = startingNumber + secondNumber 
+        startingNumber = secondNumber 
+        secondNumber = newNumber 
+    }}
+    return newNumber;
 };
 
 // Do not edit below this line
